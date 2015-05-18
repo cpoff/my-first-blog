c0ba19afb290a93cbb3066342e3432732d58cda5
cpoff <curt.poff@gmail.com>
2015-05-18 15:58:57 -0700

    Added CC form

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -21,9 +21,41 @@ article[0].appendChild(p2);
 var h1 = document.createElement('h1');
 h1.innerHTML="First post!";
 article[0].appendChild(h1);
+h1.addEventListener('click');
 
 var p1 = document.createElement('p');
 p1.innerHTML="This is the first blog post I've ever written.";
 article[0].appendChild(p1);
 
+// ------------------
+
+var p3 = document.createElement('p');
+p3.innerHTML = "<p>I take credit card payments. Hand it over, sucka!</p>";
+work[1].appendChild(p3);
+
+var makeForm = document.createElement('form');
+
+function formation(text){
+var makeLabel = document.createElement('label');
+var makeInput = document.createElement('input');
+
+makeForm.setAttribute('action', '"')
+makeLabel.setAttribute('for', 'GET-name')
+makeLabel.innerHTML=text;
+makeInput.setAttribute('name', 'text')
+makeForm.appendChild(makeLabel)
+makeForm.appendChild(makeInput)
+document.body.appendChild(makeForm)
+}
+
+formation("<br>Email: ");
+formation("<br>CC Number: ");
+formation("<br>CC Experation: ");
+formation("<br>CC Verification: ");
+formation("<br>Name: ");
+formation("<br>City: ");
+formation("<br>State: ");
+
+
 })();
+
