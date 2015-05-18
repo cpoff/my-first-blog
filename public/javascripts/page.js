9339941c41a45cdd23b6dadc4ab7369983f9644e
cpoff <curt.poff@gmail.com>
2015-05-18 10:20:47 -0700

    Second commit

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,4 +1,7 @@
 (function() {
-document.body.innerHTML = "New stuff!"
+// var grafs = document.getElementsByTagName('p');
+// grafs[0].innerHTML = "Also new";
+// var heads = document.getElementsByTagName('h1');
+// heads[0].innerHTML = "New";
 })();
 
